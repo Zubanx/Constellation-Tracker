8727969cbe0e527ceed5530d2ae2d64f9952d7b0
Javier Chavez <cjavier0315@gmail.com>
2025-10-30 16:43:17 -0400

    whatver

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,24 +1,45 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
 
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.spit(' ')[0];
+    this.url = url;
+    this.from = `Constellation Tracker <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+    return (transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    }));
+  }
+
+  send(template, subject) {
+
+  }
+  sendWelcome() {
+    this.send('welcome', 'Welcome to the Constellation Tracker!');
+  }
+};
 const sendEmail = async (options) => {
-  
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
   const mailOptions = {
     from: 'Constellation Tracker <cjavier0315@gmail.com>',
     to: options.email,
     subject: options.subject,
-    text:options.message
-  }
-  
-  await transporter.sendMail(mailOptions)
-  
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
