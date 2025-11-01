6d7f342b95ff1fb670dda0cd50387138e4f59f6b
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-01 18:36:43 -0400

    create login page

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+    return(
+        <div id="login-div">
+            <h2>LOGIN</h2>
+            <input type="text" id="login-name" placeholder="USERNAME"/>
+            <input type="password" id="login-password" placeholder="PASSWORD"/>
+        </div>
+    );
+};
+
+export default Login;
