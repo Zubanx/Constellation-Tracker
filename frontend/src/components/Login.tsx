cb16eea28823443dd8725100421f15eee99111e6
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-01 19:39:39 -0400

    add register link (doesn't do anything yet)

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -4,6 +4,7 @@ function Login() {
             <h2 className="primary-text">LOGIN</h2>
             <input type="text" id="login-name" className="secondary-text accent" placeholder="USERNAME"/>
             <input type="password" id="login-password" className="secondary-text accent" placeholder="PASSWORD"/>
+            <a href="" className="primary-text">REGISTER</a>
         </div>
     );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+// @ts-expect-error For some reason this import gives an error even though the font exists
 import "@fontsource-variable/inter";
 
 createRoot(document.getElementById('root')!).render(
