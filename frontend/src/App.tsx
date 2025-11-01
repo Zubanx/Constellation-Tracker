2d2d3c8e2ef63ed2a04c1afd69c03a45e9d521ca
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-01 18:45:28 -0400

    style text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
 import LoginPage from "./pages/LoginPage";
+import "./styles/style.css";
 
 function App() {
   return (
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,9 @@
 function Login() {
     return(
         <div id="login-div">
-            <h2>LOGIN</h2>
-            <input type="text" id="login-name" placeholder="USERNAME"/>
-            <input type="password" id="login-password" placeholder="PASSWORD"/>
+            <h2 className="primary-text">LOGIN</h2>
+            <input type="text" id="login-name" className="secondary-text" placeholder="USERNAME"/>
+            <input type="password" id="login-password" className="secondary-text" placeholder="PASSWORD"/>
         </div>
     );
 };
