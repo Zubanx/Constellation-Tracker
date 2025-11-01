059d7ab4843bd43caf5913e16e64bfa7f5ce6f92
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-01 18:53:02 -0400

    add colors

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,9 @@
 function Login() {
     return(
-        <div id="login-div">
+        <div id="login-div" className="box">
             <h2 className="primary-text">LOGIN</h2>
-            <input type="text" id="login-name" className="secondary-text" placeholder="USERNAME"/>
-            <input type="password" id="login-password" className="secondary-text" placeholder="PASSWORD"/>
+            <input type="text" id="login-name" className="secondary-text accent" placeholder="USERNAME"/>
+            <input type="password" id="login-password" className="secondary-text accent" placeholder="PASSWORD"/>
         </div>
     );
 };
