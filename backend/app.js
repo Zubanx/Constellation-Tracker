0078973889986f81868a4e1cd0e331e58fa3f24e
Miraziz <mirazizakmalov@gmail.com>
2025-11-03 21:03:18 -0500

    Fixing comment

--- backend/app.js
+++ backend/app.js
@@ -22,11 +22,16 @@ app.use('/user', userRouter);
 
 module.exports = app;
 
-
 // adding cors for browser to call our api
-const cors = require('cors')
-// allow your local Flutter dev server + future prod domain
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
-  credentials: false // we’re NOT using cookies for Option A
-}));
+const cors = require('cors');
+// allow local Flutter dev server + future prod domain
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://localhost:5174',
+    ],
+    credentials: false, // we’re NOT using cookies for Option A
+  })
+);
