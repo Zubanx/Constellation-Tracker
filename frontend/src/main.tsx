aebd3d1f0941dca91dd73cae49cc517120cecb3c
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-05 16:16:53 -0500

    switch to router based navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/style.css';
+import LoginPage from './pages/LoginPage';
+import './styles/login-style.css';
 // @ts-expect-error For some reason this import gives an error even though the font exists
 import "@fontsource-variable/inter";
 
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <LoginPage/>
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
