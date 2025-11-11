64a99e2bd2644cff9f7c201150e949308d385aab
Nathan <146783884+Corells@users.noreply.github.com>
2025-11-11 18:00:34 -0500

    add email input

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -6,6 +6,7 @@ function Register() {
     const navigate = useNavigate();
     const [message, setMessage] = useState("");
     const [registerName, setRegisterName] = useState("");
+    const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
     const [registerConfirmPass, setRegisterConfirmPass] = useState("");
 
@@ -25,6 +26,7 @@ function Register() {
         <div id="register-div" className="box">
             <h2 className="primary-text">REGISTER</h2>
             <input type="text" id="register-name" className="secondary-text accent" placeholder="USERNAME" onChange={(e) => setRegisterName(e.target.value)}/>
+            <input type="email" id="register-email" className="secondary-text accent" placeholder="EMAIL" onChange={(e) => setRegisterEmail(e.target.value)}/>
             <input type="password" id="register-password" className="secondary-text accent" placeholder="PASSWORD" onChange={(e) => setRegisterPassword(e.target.value)}/>
             <input type="password" id="confirm-password" className="secondary-text accent" placeholder="CONFIRM PASSWORD" onChange={(e) => setRegisterConfirmPass(e.target.value)}/>
             <button id="register-button" className="secondary-text accent" onClick={doRegister}>SUBMIT</button>
