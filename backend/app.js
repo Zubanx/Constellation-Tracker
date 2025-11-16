37de866f487e9a9fbe45d60704158120c030ff63
Javier Chavez <cjavier0315@gmail.com>
2025-11-16 15:15:35 -0500

    testing changes

--- backend/app.js
+++ backend/app.js
@@ -17,16 +17,25 @@ const app = express();
 // adding cors for browser to call our api
 const cors = require('cors');
 // allow local Flutter dev server + future prod domain
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'http://localhost:5174',
-    ],
-    credentials: false, // were NOT using cookies for Option A
-  })
-);
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:5174',
+      ],
+      credentials: false, // were NOT using cookies for Option A
+    })
+  );
+}else{
+  app.use(
+    cors({
+      origin: ['https://cop-433121.com'],
+      credentials: false,
+    })
+  );
+}
 
 app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
